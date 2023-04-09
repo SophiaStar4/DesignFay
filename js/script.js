586812c74cb64eb63785212994292a9092270c36
SophiaStar4 <67279669+SophiaStar4@users.noreply.github.com>
2023-04-09 11:27:43 +0300

    v1.0.001.04.09.2023
    - Add files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.slider').bxSlider({
+        auto: true,
+        speed: 1500,
+        easing: 'swing',
+        pager: false,
+        controls: false
+    });
+});
